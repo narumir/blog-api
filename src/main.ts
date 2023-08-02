91a7584136df6b78cf4b97918cf01a2b1bc95f73
narumir <narumir@narumir.io>
2023-08-02 12:42:34 +0000

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import {
   NestFactory,
 } from '@nestjs/core';
@@ -20,23 +21,25 @@ const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>("port");
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (process.env.NODE_ENV !== "production") {
-        return callback(null, true);
-      }
-      if (/^https?:\/\/(?:[^.]*\.)?narumir\.io(?:\/|$)/.test(origin)) {
-        return callback(null, true);
-      }
-      callback(new Error('Not allowed by CORS'), false);
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true,
-    optionsSuccessStatus: 204,
-    preflightContinue: false,
-  });
+  app
+    .useGlobalPipes(new ValidationPipe())
+    .use(cookieParser())
+    .use(helmet())
+    .enableCors({
+      origin: (origin, callback) => {
+        if (process.env.NODE_ENV !== "production") {
+          return callback(null, true);
+        }
+        if (/^https?:\/\/(?:[^.]*\.)?narumir\.io(?:\/|$)/.test(origin)) {
+          return callback(null, true);
+        }
+        callback(new Error('Not allowed by CORS'), false);
+      },
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      credentials: true,
+      optionsSuccessStatus: 204,
+      preflightContinue: false,
+    });
   if (process.env.NODE_ENV !== "production") {
     const swaggerConfig = new DocumentBuilder()
       .setTitle("blog api")

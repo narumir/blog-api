b295ca056bdb09ebee5c87d4d920d3750602c034
narumir <narumir@narumir.io>
2023-08-23 00:59:04 +0000

    remove code deploy configure files

--- scripts/after-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /home/ubuntu/app
-
-export PATH=~/.npm-global/bin:$PATH
-export NODE_ENV=production
-
-npm install
--- scripts/application-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /home/ubuntu/app
-
-export PATH=~/.npm-global/bin:$PATH
-export NODE_ENV=production
-
-pm2 start ./dist/main.js -i 0 --name auth

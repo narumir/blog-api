b1a2a3da69dc63034747db9d6d9ef1eaa5e58e07
narumir <narumir@narumir.io>
2023-09-04 12:11:54 +0000

    add bind

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,7 @@ const bootstrap = async () => {
     SwaggerModule.setup("api", app, document);
   }
   await app.init();
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
 };
 
 bootstrap();

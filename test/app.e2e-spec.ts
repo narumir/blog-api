e3e84e56946a0393c62c607d0f52659a9e253210
narumir <narumir@narumir.io>
2023-09-06 06:15:43 +0000

    add simple encrypt e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-
+import {
+  Test,
+  TestingModule,
+} from '@nestjs/testing';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import {
+  createPublicKey,
+} from 'crypto';
+import {
+  AppModule,
+} from './../src/app.module';
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
   });
 
-  // it('/ (GET)', () => {
-  //   return request(app.getHttpServer())
-  //     .get('/')
-  //     .expect(200)
-  //     .expect('Hello World!');
-  // });
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(404)
+  });
+
+  it('/encrypt/public-key (GET)', async () => {
+    const result = await app
+      .inject({ method: 'GET', url: '/encrypt/public-key' });
+    expect(result.statusCode).toEqual(200);
+    const { publicKey } = JSON.parse(result.payload);
+    const key = createPublicKey(publicKey)
+    expect(key).not.toBeNull();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
 });
